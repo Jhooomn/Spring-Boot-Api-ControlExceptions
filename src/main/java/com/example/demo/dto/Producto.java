429dc584b5883b31bc548d3c966aaef72f721a8c
Jhon Baron <jhooomn@gmail.com>
2019-12-12 10:18:31 -0500

    GeneratedValue and Id annotations, username = postgres

--- src/main/java/com/example/demo/dto/Producto.java
+++ src/main/java/com/example/demo/dto/Producto.java
@@ -3,6 +3,9 @@ package com.example.demo.dto;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +13,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
-public class Producto implements Serializable {
+public class Producto {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String codigo;
 	private String nombre;
 	private Double valor;
